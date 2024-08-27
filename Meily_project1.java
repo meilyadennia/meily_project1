7fd5ef50aa3847622619b9d582913ef815250691
Meily Adennia <meilyadennia@students.untidar.ac.id>
2024-08-27 08:40:43 +0700

    Project praktikum pertemuan 2

---
+++ Meily_project1.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.meily_project1;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Meily_project1 {
+
+    public static void main(String[] args) {
+        String nama = "Meily Adennia";
+        String npm = "2310506013";
+        String alamat = "Saragan Mertpyudan Magelang";
+        String no_hp = "0895392947595";
+        
+        System.out.println("Nama : " + nama);
+        System.out.println("NPM : " + npm);
+        System.out.println("Alamat : " + alamat);
+        System.out.println("No HP : " + no_hp);
+    }
+}
